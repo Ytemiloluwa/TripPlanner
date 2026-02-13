f536b82fc149f3f419f96b3ba43167998421dd45
Ytemiloluwa <ytemiloluwa@gmail.com>
2026-02-13 14:13:21 +0100

    chore: project structure

---
+++ TripPlanner/TripPlanner/App/TripPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerApp.swift
+//  TripPlanner
+//
+//  Created by Temiloluwa on 13-02-2026.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
