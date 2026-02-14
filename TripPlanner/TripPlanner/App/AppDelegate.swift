a85a704d2515b4d9f0db1c59c4a829803b4f9a05
Ytemiloluwa <ytemiloluwa@gmail.com>
2026-02-14 08:51:03 +0100

    chore: update app entry point

---
+++ TripPlanner/TripPlanner/App/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  TripPlanner
+//
+//  Created by Temiloluwa on 14-02-2026.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        return true
+    }
+}
